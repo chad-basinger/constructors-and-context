b9e73c56d345152210156463b3d765750fe747a6
Chad Basinger <chad.basingeris@gmail.com>
2021-02-25 16:36:33 -0700

    finished javascript lab day 4, unit-2

--- constructors.js
+++ constructors.js
@@ -15,6 +15,11 @@
 */
 
 // Code here
+function CarFactory(make, model){
+    this.make = make;
+    this.model = model;
+}
+
 
 ////////// PROBLEM 2 //////////
 
@@ -34,7 +39,10 @@ function Employee(name, email, hireDate) {
   */
   
   // Code here
-  
+  const bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
+
+
+
   ////////// PROBLEM 3 //////////
   
   // Do not edit the code below.
@@ -57,7 +65,24 @@ function Employee(name, email, hireDate) {
   */
   
   // Code here
-  
+  function Car(make, model, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    
+    this.moveCar = function(){
+      this.move += 10;
+      return this.move;
+    }
+  }
+
+  // Car.prototype.moveCar = function(){
+  //   return this.move += 10;
+  // }
+
+
+
+
   ////////// PROBLEM 4 //////////
   
   /*
@@ -76,7 +101,14 @@ function Employee(name, email, hireDate) {
   }
   
   // Code here
-  
+  Movie.prototype.changeRating = function(newRating){
+    avgRating = (this.rating + newRating) / 2;
+    this.rating = avgRating;
+    return this.rating;
+  }
+
+
+
   ////////// PROBLEM 5 //////////
   
   // Write a constructor function called User. This function should take in 4 parameters called name, age, email, and savedPosts in that order. Name and email will be strings, age will be a number and savedPosts will be an array of objects. These objects will each have 3 properties: id (a number), title (a string), and rating (a number between 1 and 5). These objects are the posts that the user will have saved to their account.
@@ -84,18 +116,55 @@ function Employee(name, email, hireDate) {
   // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
   
   // Code here
-  
+  function User(name, age, email, savedPosts){
+    this.name = name;
+    this.age = age;
+    this.email = email;
+    this.savedPosts = [
+      {
+        id: 1,
+        title: 'Fast 1',
+        rating: 5
+      }
+      // {
+      //   id: 2,
+      //   title: 'Fast 2',
+      //   rating: 4
+      // }
+    ]
+  }
+
+  User.prototype.addSavedPost = function(id, title, rating){
+    let newPost = {
+    id: id,
+    title: title,
+    rating: rating
+    }
+    this.savedPosts.push(newPost);
+  }
+
+  // const newPost1 = new User(2, 'Seth Fast 2', 4);
+  // newPost1.addSavedPost(2, 'Fast 2', 4)
+
   ////////// PROBLEM 6 //////////
   
   // You will be using the constructor function you just created in problem 5.
   // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
   
   // Code here
-  
+  User.prototype.removeSavedPost = function(postId){
+    let updatedPosts = this.savedPosts;
+    updatedPosts.splice(this.savedPosts.findIndex(v => v.id === postId), 1);
+  }
+
+
   ////////// PROBLEM 7 //////////
   
   // You will continue to use the constructor function you created in problem 5.
   // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
   
   // Code here
-  
+  User.prototype.changePostRating = function(id, newRating){
+    elemIndex = this.savedPosts.findIndex(v => v.id === id);
+    this.savedPosts[elemIndex].rating = newRating;
+  }
